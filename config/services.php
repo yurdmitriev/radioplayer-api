cfac7bf64bdf1017c189b41fe22eb2e0f328447b
yurdmitriev <yurdmitriev02@gmail.com>
2022-05-15 14:44:33 +0300

    fix for google auth

--- config/services.php
+++ config/services.php
@@ -33,7 +33,6 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'GOOGLE_REDIRECT_URI',
+        'redirect' => env('GOOGLE_REDIRECT_URI'),
     ],
-
 ];
