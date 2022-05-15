75f1a6cc028b8e53f046a17dd30b4ff67bf581f5
yurdmitriev <yurdmitriev02@gmail.com>
2022-05-15 23:43:33 +0300

    Implement resources for radios

--- app/Http/Controllers/RadioController.php
+++ app/Http/Controllers/RadioController.php
@@ -2,27 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\RadioBasicResource;
+use App\Http\Resources\RadioResource;
 use App\Models\Radio;
 use Illuminate\Http\Request;
 
 class RadioController extends Controller
 {
     public function index() {
-        return Radio::all();
+//        return Radio::all();
+        return RadioBasicResource::collection(Radio::all());
     }
 
     public function show($id) {
-        return Radio::findOrFail($id);
+        return new RadioResource(Radio::findOrFail($id));
     }
 
     public function store(Request $request) {
-        return Radio::create([
+        return new RadioBasicResource(Radio::create([
             'title' => $request->title,
             'description' => $request->description,
             'stream' => $request->stream,
             'logo' => $request->logo,
             'website' => $request->website
-        ]);
+        ]));
     }
 
     public function update(Request $request, $id) {
--- app/Models/Radio.php
+++ app/Models/Radio.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\Http\Resources\RadioBasicResource;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Radio extends Model
 {
@@ -13,5 +15,22 @@ class Radio extends Model
 
     public function categories() {
         return $this->belongsToMany(Category::class);
+    }
+
+    public function similarRadios() {
+        $ids = [];
+        $result = [];
+        $categories = $this->categories()->allRelatedIds()->toArray();
+
+        $res = DB::table('category_radio')->select('radio_id')
+            ->whereIn('category_id', $categories)
+            ->whereNot('radio_id', '=', $this->id)->orderByRaw('RAND()')->limit(10)->get();
+
+        foreach ($res as $r) $ids[] = $r->radio_id;
+
+        $radios = Radio::findMany($ids);
+        foreach ($radios as $radio) $result[] = new RadioBasicResource($radio);
+
+        return $result;
     }
 }
